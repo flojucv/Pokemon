787691429bd5260f12674a1e352da840bfe6b873
flojucv <flojucv@gmail.com>
2022-10-05 15:33:29 +0200

    Initial commit

---
+++ src/Pokemon.java
@@ -0,0 +1,28 @@
+public class Pokemon {
+    private String nom;
+    private double energie;
+    private double poids;
+
+    public Pokemon(){};
+    public Pokemon(String prmNom, double prmEnergie, double prmPoids) {
+        this.nom = prmNom;
+        this.energie = prmEnergie;
+        this.poids = prmPoids;
+    }
+
+    public String toString() {
+        return "Je suis le Pokémon "+this.nom+" mon poids est de "+this.poids;
+    }
+
+
+    /*-----GUETTEUR-----*/
+    protected String GetNom() {
+        return this.nom;
+    }
+    protected double GetEnergie() {
+        return this.energie;
+    }
+    protected double GetPoids() {
+        return this.poids;
+    }
+}
---
+++ src/TInsecte.java
@@ -0,0 +1,25 @@
+public class TInsecte extends Pokemon{
+    private int nbrPattes;
+    private int nbrAile;
+    private double taille;
+
+    public TInsecte() {};
+    public TInsecte(Pokemon prmPokemon, int prmNbrPattes, int prmNbrAile, double prmTaille) {
+        super(prmPokemon.GetNom(), prmPokemon.GetEnergie(), prmPokemon.GetPoids());
+        this.nbrAile = prmNbrAile;
+        this.nbrPattes = prmNbrPattes;
+        this.taille = prmTaille;
+    }
+
+    public double vitesseSol() {
+        return (this.nbrPattes * this.taille * 3);
+    }
+
+    public  double vitesseAir() {
+        return (this.nbrAile * this.taille * 10);
+    }
+
+    public String toString() {
+        return super.toString() + "ma vitesse au sol est de " + vitesseSol() + "km/h, en vol est de " + vitesseAir()+", " + this.nbrPattes + "pattes, ma taille est de " + this.taille + "d";
+    }
+}
